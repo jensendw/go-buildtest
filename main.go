c16bbeac874e77596bb3674121c36a44766664d1
Daniel Jensen <jensendw@gmail.com>
2018-04-09 16:59:31 -0700

    adding godeps

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	consulapi "github.com/hashicorp/consul/api"
 )
 
 func main() {
 	fmt.Println("hello world")
+}
+
+func consulFunction() {
+	config := consulapi.DefaultConfig()
+	consul, _ := consulapi.NewClient(config)
+	fmt.Println(consul)
 }
